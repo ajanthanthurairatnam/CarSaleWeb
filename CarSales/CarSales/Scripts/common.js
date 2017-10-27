d6cf9b0945c13ae1735de2f670656d8e071f11a1
ajanthan <ajanthan.kt@gmail.com>
2017-10-28 08:23:22 +1100

    Changes To UI

---
+++ CarSales/CarSales/Scripts/common.js
@@ -0,0 +1,13 @@
+﻿$(function () {
+    $('.content').mouseover(function () {
+        $(this).css("cursor", "pointer");
+        $(this).animate({ width: "500px" }, 'slow');
+    });
+
+    $('.content').mouseout(function ()
+     {
+
+         $(this).animate({width: "200px"}, 'slow');
+ 
+     });
+});
