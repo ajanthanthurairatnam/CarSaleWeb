45f4ecff2bd324cec60f44489d01f85eb3da776b
Samitha <samithaslk@gmail.com>
2017-10-30 14:21:56 +0530

    changed vehicle index

---
+++ CarSales/CarSales/Scripts/common.js
@@ -0,0 +1,12 @@
+﻿$(function () {
+    $('.content').mouseover(function () {
+        $(this).css("cursor", "pointer");
+        $(this).animate({ width: "500px" }, 'slow');
+    });
+
+    $('.content').mouseout(function () {
+
+        $(this).animate({ width: "200px" }, 'slow');
+
+    });
+});
